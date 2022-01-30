416409c3bf2c90a4e90270db8a65887ddc45bcc3
yushoyamaguchi <ysh.824@outlook.jp>
2022-01-30 12:10:06 +0900

    many

---
+++ local1/bgp.h
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+
+#ifndef _BGP_H_
+#define _BGP_H_
+
+#define BGP_HD_LEN 19
+#define BGP_OPEN_LEN 29
+#define MARKER_NUM 16
+#define VERSION 4
+#define TYPE_OPEN 1
+#define TYPE_UPDATE 2
+#define TYPE_NOTIF 3
+#define TYPE_KEEP 4
+
+struct bgp_hd
+  {
+    uint8_t marker[16];
+    uint16_t len;
+    uint8_t type;
+  };
+
+
+struct bgp_open
+  {
+    uint8_t marker[16];
+    uint16_t len;
+    uint8_t type;
+    uint8_t version;
+    uint16_t myas;
+    uint16_t holdtime;
+    uint32_t id;
+    uint8_t opt_len;
+  };
+
+
+struct bgp_open_opt
+  {
+    uint8_t marker[16];
+    uint16_t len;
+    uint8_t type;
+    uint8_t version;
+    uint16_t myas;
+    uint16_t holdtime;
+    uint32_t id;
+    uint8_t opt_len;
+    uint8_t opt[46];
+  };
+
+struct bgp_update
+    {
+      uint8_t marker[16];
+      uint16_t len;
+      uint8_t type;
+      uint16_t withdrawn_route_len;
+      uint8_t contents[48];
+    };
+
+enum PeerState {
+  Idle,
+  Connect,
+  Active,
+  OpenSent,
+  OpenConfirm,
+  Established
+} ;
+
+struct Peer
+{
+  int remote_asn;
+	int timer_connect;
+  enum PeerState state;
+
+};
+
+struct BGP {
+	int asn;
+	struct Peer peers[256];
+};
+
+
+
+
+
+
+#endif
---
+++ local1/main.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bgp.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int exec_peer(char *addr);
+
+
+int main(int argc,char **argv)
+{
+  char addr[127];
+
+  //printf("input ip address");
+  //scanf("%s", addr);
+  exec_peer("10.255.1.2");
+  printf("end\n");
+
+
+  return 0;
+}
